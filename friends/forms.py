8f80c393f6419dd25c9e6413674ddb44821aaad0
Tiberiu Ichim <tibi@pixelblaster.ro>
2009-11-30 15:02:03 -0500

    Added internationalization support

--- friends/forms.py
+++ friends/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.conf import settings
+from django.utils.translation import ugettext as _
 
 from django.contrib.auth.models import User
 
@@ -26,33 +27,33 @@ class UserForm(forms.Form):
 if EmailAddress:
     class JoinRequestForm(forms.Form):
         
-        email = forms.EmailField(label="Email", required=True, widget=forms.TextInput(attrs={'size':'30'}))
-        message = forms.CharField(label="Message", required=False, widget=forms.Textarea(attrs = {'cols': '30', 'rows': '5'}))
+        email = forms.EmailField(label=_("Email"), required=True, widget=forms.TextInput(attrs={'size':'30'}))
+        message = forms.CharField(label=_("Message"), required=False, widget=forms.Textarea(attrs = {'cols': '30', 'rows': '5'}))
         
         def clean_email(self):
             # @@@ this assumes email-confirmation is being used
             self.existing_users = EmailAddress.objects.get_users_for(self.cleaned_data["email"])
             if self.existing_users:
-                raise forms.ValidationError(u"Someone with that email address is already here.")
+                raise forms.ValidationError(_(u"Someone with that email address is already here."))
             return self.cleaned_data["email"]
         
         def save(self, user):
             join_request = JoinInvitation.objects.send_invitation(user, self.cleaned_data["email"], self.cleaned_data["message"])
-            user.message_set.create(message="Invitation to join sent to %s" % join_request.contact.email)
+            user.message_set.create(message=_("Invitation to join sent to %(email)s") % join_request.contact.email)
             return join_request
 
 
 class InviteFriendForm(UserForm):
     
     to_user = forms.CharField(widget=forms.HiddenInput)
-    message = forms.CharField(label="Message", required=False, widget=forms.Textarea(attrs = {'cols': '20', 'rows': '5'}))
+    message = forms.CharField(label=_("Message"), required=False, widget=forms.Textarea(attrs = {'cols': '20', 'rows': '5'}))
     
     def clean_to_user(self):
         to_username = self.cleaned_data["to_user"]
         try:
             User.objects.get(username=to_username)
         except User.DoesNotExist:
-            raise forms.ValidationError(u"Unknown user.")
+            raise forms.ValidationError(_(u"Unknown user."))
             
         return self.cleaned_data["to_user"]
     
@@ -60,11 +61,11 @@ class InviteFriendForm(UserForm):
         to_user = User.objects.get(username=self.cleaned_data["to_user"])
         previous_invitations_to = FriendshipInvitation.objects.invitations(to_user=to_user, from_user=self.user)
         if previous_invitations_to.count() > 0:
-            raise forms.ValidationError(u"Already requested friendship with %s" % to_user.username)
+            raise forms.ValidationError(_(u"Already requested friendship with %(username)s") % to_user.username)
         # check inverse
         previous_invitations_from = FriendshipInvitation.objects.invitations(to_user=self.user, from_user=to_user)
         if previous_invitations_from.count() > 0:
-            raise forms.ValidationError(u"%s has already requested friendship with you" % to_user.username)
+            raise forms.ValidationError(_(u"%(username)s has already requested friendship with you") % to_user.username)
         return self.cleaned_data
     
     def save(self):
@@ -75,5 +76,5 @@ class InviteFriendForm(UserForm):
         if notification:
             notification.send([to_user], "friends_invite", {"invitation": invitation})
             notification.send([self.user], "friends_invite_sent", {"invitation": invitation})
-        self.user.message_set.create(message="Friendship requested with %s" % to_user.username) # @@@ make link like notification
+        self.user.message_set.create(message=_("Friendship requested with %(username)s") % to_user.username) # @@@ make link like notification
         return invitation
